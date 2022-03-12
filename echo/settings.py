cb46e09287e189ec47cc6933ea9ec63f4ca60473
aathira <aathirapookkad@gmail.com>
2022-03-12 13:36:33 +0530

    allowed host

--- echo/settings.py
+++ echo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*@a=#y%t$_1p46h2fq&oc(!$dbh%an*ajd6r6n@eo5=g3xjr3(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
